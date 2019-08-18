f08527d8304e25506a7448ce15611a17dc4bc98e
SergeyVN94 <blakdgak1@yandex.ru>
2019-08-18 19:01:05 +0300

    Added room-reservation-form.

--- src/lib.blocks/dropdown/dropdown.js
+++ src/lib.blocks/dropdown/dropdown.js
@@ -1,5 +1,4 @@
 import $ from "jquery";
-import './dropdown_guest';
 
 $('.dropdown .dropdown-head').on('click', function() {
     $(this).parent('.dropdown').toggleClass('dropdown_expend');
--- src/lib.blocks/index.js
+++ src/lib.blocks/index.js
@@ -4,3 +4,4 @@ import './dropdown-date/dropdown-date';
 import './range-slider/range-slider';
 import './dropdown-item/dropdown-item'
 import './expandable-checkbox-list/expandable-checkbox-list';
+import './dropdown-guest/dropdown-guest';
