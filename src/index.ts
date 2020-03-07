017c8a67a6f0e7ca1ab6c885ffa37fea653eb63d
SergeyVN94 <blakdgak1@yandex.ru>
2020-03-07 19:42:40 +0300

    Issues #9. Concat lines.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ const importAll = function importAll(resolve: __WebpackModuleApi.RequireContext)
 };
 
 require('./styles/fonts.scss');
-importAll(require.context('./desktop.blocks', true, /.ts$/));
-importAll(require.context('./desktop.blocks', true, /.(sa|sc|c)ss$/));
+importAll(require.context('./desktop.blocks', true, /.(((sa|sc|c)ss)|ts)$/));
 importAll(require.context('./pages', true, /.(sa|sc|c)ss$/));
