220ba2f0732d612e09992ec5d209ddb2c53295e7
SergeyVN94 <blakdgak1@yandex.ru>
2020-03-12 17:56:48 +0300

    Scripts for menu-item-expandable fixed. Issues #13.

--- src/desktop.blocks/menu-item-expandable/classes.ts
+++
@@ -1,7 +0,0 @@
-const CLASSES = {
-    MENU_ITEM: 'js-menu-item-expandable',
-    BTN_EXPAND: 'js-menu-item-expandable__btn-expand',
-    EXPANDED: 'menu-item-expandable_expanded',
-};
-
-export default CLASSES;
--- src/desktop.blocks/menu-item-expandable/menu-item-expandable.ts
+++ src/desktop.blocks/menu-item-expandable/menu-item-expandable.ts
@@ -1,1 +1,14 @@
-import './menu-item-expandable__btn-expand';
+const MENU_ITEM_EXPANDABLE_CLASSES = {
+    MENU_ITEM: 'js-menu-item-expandable',
+    BTN_EXPAND: 'js-menu-item-expandable__btn-expand',
+    EXPANDED: 'menu-item-expandable_expanded',
+};
+
+const handleMenuItemClick = function handleMenuItemClick(ev: JQuery.MouseEventBase): void {
+    $(ev.delegateTarget).toggleClass(MENU_ITEM_EXPANDABLE_CLASSES.EXPANDED);
+};
+
+$(`.${MENU_ITEM_EXPANDABLE_CLASSES.MENU_ITEM}`).on(
+    'click.menu-item-expandable.expand',
+    handleMenuItemClick
+);
