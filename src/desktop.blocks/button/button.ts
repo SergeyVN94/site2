522c0ed87f3fb683fe205a1fb95ad2084bbdebe3
SergeyVN94 <blakdgak1@yandex.ru>
2020-03-12 16:39:17 +0300

    Scripts for button fixed. Issues #13.

--- src/desktop.blocks/button/button.ts
+++ src/desktop.blocks/button/button.ts
@@ -1,1 +1,32 @@
-import './plugin/plugin';
+const BUTTON_CLASSES = {
+    DISABLE: 'button_disable',
+    HIDDEN: 'button_hidden',
+};
+
+$.fn.button = function buttonPlugin(
+    this: JQuery,
+    command: 'disable' | 'hidden',
+    args: boolean = null,
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+): boolean | JQuery | any {
+    switch (command) {
+        case 'disable':
+            if (args === null) {
+                return this.hasClass(BUTTON_CLASSES.DISABLE);
+            }
+
+            this.toggleClass(BUTTON_CLASSES.DISABLE, args);
+            return this;
+
+        case 'hidden':
+            if (args === null) {
+                return this.hasClass(BUTTON_CLASSES.HIDDEN);
+            }
+
+            this.toggleClass(BUTTON_CLASSES.HIDDEN, args);
+            return this;
+
+        default:
+            throw new Error(`Unknown command '${command}'`);
+    }
+};
