09614bdd2c5a23c2ddb066daac4110fba6b5ce76
SergeyVN94 <blakdgak1@yandex.ru>
2020-03-16 15:31:58 +0300

    Common styles are collected in the main.scss file.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ const importAll = function importAll(resolve: __WebpackModuleApi.RequireContext)
     resolve.keys().forEach(resolve);
 };
 
-require('./styles/fonts.scss');
 importAll(require.context('./desktop.blocks', true, /.(((sa|sc|c)ss)|ts)$/));
 importAll(require.context('./pages', true, /.(sa|sc|c)ss$/));
+importAll(require.context('./styles', true, /.(sa|sc|c)ss$/));
