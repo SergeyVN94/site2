2e30914814719ff21ebed5d99e5e258b091ac9fd
SergeyVN94 <blakdgak1@yandex.ru>
2020-03-22 15:52:57 +0300

    Fixed bug with date display. Issue #17.

--- src/desktop.blocks/date-range/dateRange.ts
+++ src/desktop.blocks/date-range/dateRange.ts
@@ -57,7 +57,7 @@ class DateRange {
 
     private _dateToString(date: Date): string {
         const day = String(date.getDate()).padStart(2, '0');
-        const month = String(date.getMonth()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
         const year = date.getFullYear();
 
         return `${day}.${month}.${year}`;
