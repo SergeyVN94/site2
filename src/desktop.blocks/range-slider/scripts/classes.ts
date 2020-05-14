0eb8dcdfada98ee90906785764d1c1e3cc84f508
SergeyVN94 <blakdgak1@yandex.ru>
2020-05-14 18:11:52 +0300

    Some bugs fixed.

--- src/desktop.blocks/range-slider/scripts/classes.ts
+++ src/desktop.blocks/range-slider/scripts/classes.ts
@@ -1,6 +1,6 @@
 const enum RANGE_SLIDER_CLASSES {
   SLIDER = 'js-range-slider',
-  LINE = 'js-range-slider__line',
+  LINE = 'js-range-slider__body',
   POINT = 'js-range-slider__point',
   BG_LINE = 'js-range-slider__bg-line',
   VALUES_OUT = 'js-range-slider__out-range',
