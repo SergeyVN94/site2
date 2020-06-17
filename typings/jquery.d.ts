7f3dc826c920b1107bd3b1aa044af08ecc268ef5
SergeyVN94 <blakdgak1@yandex.ru>
2020-06-17 22:31:13 +0300

    Fixed indent.

--- typings/jquery.d.ts
+++ typings/jquery.d.ts
@@ -1,21 +1,21 @@
 type CounterValue = {
-    text: string;
-    value?: number;
+  text: string;
+  value?: number;
 };
 
 // eslint-disable-next-line @typescript-eslint/interface-name-prefix
 interface JQuery {
-    dropdownHead(command: 'set-theme' | 'remove-theme', theme: string): JQuery;
-    dropdownHead(command: 'has-theme', theme: string): boolean;
-    dropdownHead(command: 'text', text: string): JQuery;
-    dropdownHead(command: 'text'): string;
-    dropdownCounter(command: 'value'): number;
-    dropdownCounter(command: 'value', value: number): JQuery;
-    dropdownCounter(command: 'group'): string;
-    dropdownCounter(command: 'reset'): JQuery;
-    calendar(command: 'select-date', args: 'start' | 'end'): JQuery;
-    button(command: 'disable'): boolean;
-    button(command: 'disable', state: boolean): JQuery;
-    button(command: 'hidden'): boolean;
-    button(command: 'hidden', state: boolean): JQuery;
+  dropdownHead(command: 'set-theme' | 'remove-theme', theme: string): JQuery;
+  dropdownHead(command: 'has-theme', theme: string): boolean;
+  dropdownHead(command: 'text', text: string): JQuery;
+  dropdownHead(command: 'text'): string;
+  dropdownCounter(command: 'value'): number;
+  dropdownCounter(command: 'value', value: number): JQuery;
+  dropdownCounter(command: 'group'): string;
+  dropdownCounter(command: 'reset'): JQuery;
+  calendar(command: 'select-date', args: 'start' | 'end'): JQuery;
+  button(command: 'disable'): boolean;
+  button(command: 'disable', state: boolean): JQuery;
+  button(command: 'hidden'): boolean;
+  button(command: 'hidden', state: boolean): JQuery;
 }
