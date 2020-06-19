3f465b6a4d152735368bdcd75c44e7a3d61bcc10
SergeyVN94 <blakdgak1@yandex.ru>
2020-06-19 15:25:26 +0300

    Added new methods for highlighting the path in room-impressions. Issue #49.

--- src/desktop.blocks/room-impressions/roomImpressions.ts
+++ src/desktop.blocks/room-impressions/roomImpressions.ts
@@ -2,12 +2,39 @@ const enum ROOM_IMPRESSION_CLASSES {
   DIAGRAM = 'js-room-impressions__diagram',
   SVG = 'js-room-impressions__diagram-image',
   VALUE_OUT = 'js-room-impressions__value',
+  HELP_ITEM = 'js-room-impressions__help-item',
+  ROOM_IMPRESSIONS = 'js-room-impressions',
 }
 
-$(`.${ROOM_IMPRESSION_CLASSES.DIAGRAM}`).each((index, element) => {
-  const $diagram = $(element);
+const selectPath = function selectPath($path: JQuery, $diagram: JQuery, select = true): void {
+  $path.attr('d', $path.data(select ? 'path-expanded' : 'path'));
+  $path.attr('stroke-width', $path.data(select ? 'stroke-width-expanded' : 'stroke-width'));
+
+  $diagram.removeClass((index, classes) => classes.split(' ').filter((className) => className.includes('diagram_theme_')).join(' '));
+
+  if (select) $diagram.addClass(`room-impressions__diagram_theme_${$path.data('theme')}`);
+};
+
+$(`.${ROOM_IMPRESSION_CLASSES.ROOM_IMPRESSIONS}`).each((index, element) => {
+  const $roomImpressions = $(element);
+  const $diagram = $roomImpressions.find(`.${ROOM_IMPRESSION_CLASSES.DIAGRAM}`);
   const $out = $diagram.find(`.${ROOM_IMPRESSION_CLASSES.VALUE_OUT}`);
   const totalValue = $out.text();
+
+  $roomImpressions
+    .find(`.${ROOM_IMPRESSION_CLASSES.HELP_ITEM}`)
+    .on('mouseover.roomImpression.selectPathWithHelp', (ev: JQuery.MouseOverEvent) => {
+      const $path = $diagram.find(`svg path[data-theme='${ev.currentTarget.dataset.theme}']`);
+
+      if ($path.length) {
+        $out.text($path.data('value'));
+        selectPath($path, $diagram);
+      }
+    }).on('mouseout.roomImpression.unselectPathWithHelp', (ev: JQuery.MouseOutEvent) => {
+      const $path = $diagram.find(`svg path[data-theme='${ev.currentTarget.dataset.theme}']`);
+      $out.text(totalValue);
+      if ($path.length) selectPath($path, $diagram, false);
+    });
 
   $diagram
     .find(`.${ROOM_IMPRESSION_CLASSES.SVG}`)
@@ -15,28 +42,10 @@ $(`.${ROOM_IMPRESSION_CLASSES.DIAGRAM}`).each((index, element) => {
     .on('mouseover.roomImpression.selectPath', (ev: JQuery.MouseOverEvent) => {
       const $path = $(ev.target);
       $out.text($path.data('value'));
-
-      const tmpPath = $path.attr('d');
-      $path.attr('d', $path.data('path-expanded'));
-      $path.data('path-expanded', tmpPath);
-
-      const tmpStrokeWidth = $path.attr('stroke-width');
-      $path.attr('stroke-width', $path.data('stroke-width-expanded'));
-      $path.data('stroke-width-expanded', tmpStrokeWidth);
-
-      $diagram.addClass(`room-impressions__diagram_theme_${$path.data('theme')}`);
+      selectPath($path, $diagram);
     }).on('mouseout.roomImpression.unselectPath', (ev: JQuery.MouseOutEvent) => {
-      $out.text(totalValue);
-
       const $path = $(ev.target);
-      const tmpPath = $path.attr('d');
-      $path.attr('d', $path.data('path-expanded'));
-      $path.data('path-expanded', tmpPath);
-
-      const tmpStrokeWidth = $path.attr('stroke-width');
-      $path.attr('stroke-width', $path.data('stroke-width-expanded'));
-      $path.data('stroke-width-expanded', tmpStrokeWidth);
-
-      $diagram.removeClass(`room-impressions__diagram_theme_${$path.data('theme')}`);
+      $out.text(totalValue);
+      selectPath($path, $diagram, false);
     });
 });
