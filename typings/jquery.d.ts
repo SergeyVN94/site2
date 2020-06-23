54790e555747fa1752670f69ae515fede4473064
SergeyVN94 <blakdgak1@yandex.ru>
2020-06-23 17:30:24 +0300

    Unused interfaces removed.

--- typings/jquery.d.ts
+++ typings/jquery.d.ts
@@ -5,14 +5,6 @@ type CounterValue = {
 
 // eslint-disable-next-line @typescript-eslint/interface-name-prefix
 interface JQuery {
-  dropdownHead(command: 'set-theme' | 'remove-theme', theme: string): JQuery;
-  dropdownHead(command: 'has-theme', theme: string): boolean;
-  dropdownHead(command: 'text', text: string): JQuery;
-  dropdownHead(command: 'text'): string;
-  dropdownCounter(command: 'value'): number;
-  dropdownCounter(command: 'value', value: number): JQuery;
-  dropdownCounter(command: 'group'): string;
-  dropdownCounter(command: 'reset'): JQuery;
   calendar(command: 'select-date', args: 'start' | 'end' | 'auto'): JQuery;
   button(command: 'disable'): boolean;
   button(command: 'disable', state: boolean): JQuery;
