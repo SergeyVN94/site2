693d459e5958603f63dc5fbaefbeaeef29c49e07
SergeyVN94 <blakdgak1@yandex.ru>
2020-06-23 17:33:48 +0300

    Interfaces updated.

--- typings/jquery.d.ts
+++ typings/jquery.d.ts
@@ -6,8 +6,6 @@ type CounterValue = {
 // eslint-disable-next-line @typescript-eslint/interface-name-prefix
 interface JQuery {
   calendar(command: 'select-date', args: 'start' | 'end' | 'auto'): JQuery;
-  button(command: 'disable'): boolean;
-  button(command: 'disable', state: boolean): JQuery;
-  button(command: 'hidden'): boolean;
-  button(command: 'hidden', state: boolean): JQuery;
+  button(command: 'disable' | 'hidden'): boolean;
+  button(command: 'disable' | 'hidden', state: boolean): JQuery;
 }
