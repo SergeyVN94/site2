79d74c1a75a3a5694815d7c4557d21e6dcffbc16
SergeyVN94 <blakdgak1@yandex.ru>
2020-06-26 21:27:03 +0300

    Fixed menu.

--- src/desktop.blocks/menu/menu.ts
+++ src/desktop.blocks/menu/menu.ts
@@ -1,14 +1,34 @@
 const enum MENU_CLASSES {
-  MENU_ITEM_EXPANDABLE = 'js-menu__item_expandable',
-  BTN_EXPAND = 'js-menu__btn-expand',
+  ITEM_EXPANDABLE = 'js-menu__item_expandable',
   ITEM_EXPANDED = 'menu__item_expanded',
+  ITEM_TEXT = 'js-menu__item-text',
 }
 
-const handleMenuItemClick = function handleMenuItemClick(ev: JQuery.MouseEventBase): void {
-  $(ev.delegateTarget).toggleClass(MENU_CLASSES.ITEM_EXPANDED);
+const handleDocumentClick = function handleDocumentClick(
+  this: JQuery,
+  _ev: { originalEvent: { path: Element[] } },
+): void {
+  const isClickInItem = _ev.originalEvent.path.some((element) => {
+    if ('classList' in element) return element.classList.contains(MENU_CLASSES.ITEM_EXPANDABLE);
+    return false;
+  });
+
+  if (!isClickInItem) {
+    this.removeClass(MENU_CLASSES.ITEM_EXPANDED);
+    $(document).off('click.menu__item_expandable.focusout');
+  }
 };
 
-$(`.${MENU_CLASSES.MENU_ITEM_EXPANDABLE}`).on(
+const handleMenuItemClick = function handleMenuItemClick(ev: JQuery.MouseEventBase): void {
+  const $item = $(ev.delegateTarget).toggleClass(MENU_CLASSES.ITEM_EXPANDED);
+
+  if ($item.hasClass(MENU_CLASSES.ITEM_EXPANDED)) {
+    $(document).on('click.menu__item_expandable.focusout', handleDocumentClick.bind($item));
+  }
+};
+
+$(`.${MENU_CLASSES.ITEM_EXPANDABLE}`).on(
   'click.menu__item_expandable.expand',
+  `.${MENU_CLASSES.ITEM_TEXT}`,
   handleMenuItemClick,
 );
